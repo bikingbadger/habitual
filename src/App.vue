d82dc7c9ef75773f7104d33e7293e9669e3e7344
Hilton Meyer <development@hiltonmeyer.com>
2020-12-23 17:09:43 +0200

    feat(setup): Create vue app

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>{{ name }}</h1>
+  <ul>
+    <li v-for="habit in habits">
+      
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Habitual',
+      habits: [{name: 'Read '},
+        {name: 'Meditate'},
+        {name: 'Stimulants'}
+        ]
+    };
+  },
+  components: {},
+};
+</script>
+
+<style></style>
