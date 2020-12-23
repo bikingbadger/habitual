17b555a0cccfeb86f756146e345a93948aa32c2e
Hilton Meyer <development@hiltonmeyer.com>
2020-12-23 17:49:12 +0200

    feat(style): Add style to habit

--- src/components/habits/Habit.vue
+++ src/components/habits/Habit.vue
@@ -2,6 +2,13 @@
   <li>
     <input type="checkbox" :id="id" :name="name" :value="name" />
     <label :for="name">{{ name }}</label>
+    <div class="history-box complete">x</div>
+    <div class="history-box complete">x</div>
+    <div class="history-box">o</div>
+    <div class="history-box complete">x</div>
+    <div class="history-box">o</div>
+    <div class="history-box complete">x</div>
+    <div class="history-box complete">x</div>
   </li>
 </template>
 
@@ -13,3 +20,29 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+li {
+  display: grid;
+  grid-template-columns: 1fr 5fr repeat(7, 1fr);
+  grid-auto-flow: column;
+  justify-items: center;
+  align-items: center;
+}
+
+input[type='checkbox']:checked + label {
+  color: #ccc;
+  font-style: italic;
+  text-decoration: line-through;
+}
+
+.history-box {
+  padding: 1rem;
+  border: 2px blue solid;
+  border-radius: 0.3rem;
+}
+
+.complete {
+  background-color: rgb(127, 214, 255);
+}
+</style>
