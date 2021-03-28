c9c514e0aef568632cb72d706a30328b989c7a48
Hilton Meyer <development@hiltonmeyer.com>
2021-03-28 19:32:15 +0300

    feat(auth): Add auth to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,14 @@
 import { createStore } from 'vuex';
+import authStore from './auth/authIndex';
 
 const store = createStore({
   state() {},
   mutations: {},
   actions: {},
   getters: {},
+  modules: {
+    auth: authStore,
+  },
 });
 
 export default store;
