2c7db541278ac3934ca19af3431564d78ac86fa9
Hilton Meyer <development@hiltonmeyer.com>
2021-03-28 18:50:24 +0300

    feat(setup): Create store

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import firebase from '@/utils/firebase.js';
+import store from '@/store';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+let app;
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = createApp(App);
+
+    app.use(firebase);
+    app.use(store);
+
+    app.mount('#app');
+  }
+});
