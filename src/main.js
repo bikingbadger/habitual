91ae76a0ec5a265808239cc4b0162074ce875e32
Hilton Meyer <development@hiltonmeyer.com>
2021-03-28 19:59:14 +0300

    feat(setup): Add router

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import { createApp } from 'vue';
 import firebase from '@/utils/firebase.js';
 import store from '@/store';
 import App from './App.vue';
+import router from './router'
 
 let app;
 
 firebase.auth().onAuthStateChanged(() => {
   if (!app) {
-    app = createApp(App);
+    app = createApp(App).use(router);
 
     app.use(firebase);
     app.use(store);
